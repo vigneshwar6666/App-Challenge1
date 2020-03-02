92abb4b27d95b1b87602dce41a104c1ced3fa398
vigneshwar6666 <vigneshwar6666@gmail.com>
2020-03-02 11:06:01 -0600

    Commit Changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function myFunction() {
-    var a = document.getElementById("txt1").value;
-   var b = document.getElementById("txt2").value;
-   var c = +a + +b;
+    var a = document.getElementById("text1").value;
+   var b = document.getElementById("text2").value;
+   var c = Math.max(a.length,b.length);
    document.getElementById("sum").innerHTML = c;
  }
